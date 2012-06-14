9e2daecfe46d04b736fb700e994e2a7a23d73f35
tschaub <tim.schaub@gmail.com>
2012-06-14 12:39:37 -0600

    Map projection and user projection.

--- src/ol/Map.js
+++ src/ol/Map.js
@@ -4,21 +4,60 @@
  */
 
 ol.Map = ol.Class(ol.Base, {
+    
+    defaults: {
+        projection: "EPSG:900913",
+        userProjection: "EPSG:4326"
+    },
+    
     center: function(loc) {
         if (arguments.length === 0) {
-            return this.config.center;
+            return this.config.center.transform(this.userProjection());
         }
         if (!(loc instanceof ol.Location)) {
             loc = new ol.Location(loc);
         }
-        this.config.center = loc;
+        if (!loc.projection()) {
+            loc.projection(this.userProjection());
+        }
+        this.config.center = loc.transform(this.projection());
         return this;
     },
+
     zoom: function(level) {
         if (arguments.length === 0) {
             return this.config.zoom;
         }
         this.config.zoom = level;
         return this;
+    },
+    
+    projection: function(proj) {
+        if (arguments.length === 0) {
+            if (!("projection" in this.config)) {
+                this.projection(this.defaults.projection);
+            }
+            return this.config.projection;
+        }
+        if (!(proj instanceof ol.Projection)) {
+            proj = new ol.Projection(proj);
+        }
+        this.config.projection = proj;
+        return this;
+    },
+    
+    userProjection: function(proj) {
+        if (arguments.length === 0) {
+            if (!("userProjection" in this.config)) {
+                this.userProjection(this.defaults.userProjection);
+            }
+            return this.config.userProjection;
+        }
+        if (!(proj instanceof ol.Projection)) {
+            proj = new ol.Projection(proj);
+        }
+        this.config.userProjection = proj;
+        return this;
     }
+
 });
--- test/spec/ol/Map.js
+++ test/spec/ol/Map.js
@@ -21,16 +21,16 @@ describe("ol.Map", function() {
         map.center([-110, 45]);
         
         center = map.center();
-        expect(center[0]).toBe(-110);
-        expect(center[1]).toBe(45);
+        expect(center[0].toFixed(3)).toBe("-110.000");
+        expect(center[1].toFixed(3)).toBe("45.000");
         expect(center instanceof ol.Location).toBe(true);
         
         // one step
         map = new ol.Map({center: [-111, 46]});
 
         center = map.center();
-        expect(center[0]).toBe(-111);
-        expect(center[1]).toBe(46);
+        expect(center[0].toFixed(3)).toBe("-111.000");
+        expect(center[1].toFixed(3)).toBe("46.000");
         expect(center instanceof ol.Location).toBe(true);
         
         // more verbose
@@ -39,8 +39,8 @@ describe("ol.Map", function() {
         });
         
         center = map.center();
-        expect(center[0]).toBe(-112);
-        expect(center[1]).toBe(47);
+        expect(center[0].toFixed(3)).toBe("-112.000");
+        expect(center[1].toFixed(3)).toBe("47.000");
         expect(center instanceof ol.Location).toBe(true);
         
     });
@@ -53,8 +53,8 @@ describe("ol.Map", function() {
 
         center = map.center();
         zoom = map.zoom();
-        expect(center[0]).toBe(1);
-        expect(center[1]).toBe(2);
+        expect(center[0].toFixed(3)).toBe("1.000");
+        expect(center[1].toFixed(3)).toBe("2.000");
         expect(zoom).toBe(3);
         
         // all at once
@@ -65,9 +65,56 @@ describe("ol.Map", function() {
 
         center = map.center();
         zoom = map.zoom();
-        expect(center[0]).toBe(4);
-        expect(center[1]).toBe(5);
+        expect(center[0].toFixed(3)).toBe("4.000");
+        expect(center[1].toFixed(3)).toBe("5.000");
         expect(zoom).toBe(6);
+        
+    });
+    
+    it("has a default projection", function() {
+        
+        var map = new ol.Map();
+        var proj = map.projection();
+        
+        expect(proj instanceof ol.Projection).toBe(true);
+        expect(proj.code()).toBe("EPSG:900913");
+        
+    });
+
+    it("allows projection to be set", function() {
+        var proj;
+        
+        // at construction
+        var map = new ol.Map({projection: "EPSG:4326"});
+        proj = map.projection();
+        
+        expect(proj instanceof ol.Projection).toBe(true);
+        expect(proj.code()).toBe("EPSG:4326");
+        
+        // after construction
+        map.projection("EPSG:900913");
+        proj = map.projection();
+        
+        expect(proj instanceof ol.Projection).toBe(true);
+        expect(proj.code()).toBe("EPSG:900913");
+        
+    });
+    
+    it("allows a user projection to be set", function() {
+        var proj;
+
+        var map = new ol.Map();
+        proj = map.userProjection();
+        
+        expect(proj instanceof ol.Projection).toBe(true);
+        expect(proj.code()).toBe("EPSG:4326");
+        
+        map.center([10, 20]);
+        
+        map.userProjection("EPSG:900913");
+        var center = map.center();
+        expect(center.x().toFixed(3)).toBe("1113194.908");
+        expect(center.y().toFixed(3)).toBe("2273030.927");
         
     });
     
